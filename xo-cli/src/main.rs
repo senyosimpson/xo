8a6d9410416f21ba1e43d7ab3f55bfc15ed886d8
senyo simpson <simpsonsenyo@gmail.com>
2024-04-27 21:05:26 +0200

    add cli

--- xo-cli/src/main.rs
+++ xo-cli/src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Cmd,
+}
+
+#[derive(Subcommand, Debug)]
+enum Cmd {
+    Log { message: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Cmd::Log { message } => {
+            let client = reqwest::blocking::Client::new();
+
+            let mut body = HashMap::with_capacity(2);
+            body.insert("message", message);
+
+            let url = std::env::var("XO_API_URL").expect("XO_API_URL not set");
+
+            let _ = client.post(url).json(&body).send();
+            println!("message logged")
+        }
+    }
 }
